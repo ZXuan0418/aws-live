bcaa100a16987beb025c81af92813440d7399abc
ZXuan0418 <77720543+ZXuan0418@users.noreply.github.com>
2022-09-26 15:56:23 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -80,6 +80,55 @@ def AddEmp():
     print("all modification done...")
     return render_template('AddEmpOutput.html', name=emp_name)
 
+#update employee code
+@app.route("/updateemp", methods=['GET', 'POST'])
+def UpdateEmp():
+    emp_id = request.form['emp_id']
+
+    get_sql = "SELECT * FROM employee WHERE emp_id = %s"
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    pri_skill = request.form['pri_skill']
+    location = request.form['location']
+    emp_image_file = request.files['emp_image_file'] 
+
+    update_sql = "UPDATE INTO employee SET first_name = %s, last_name = %s, pri_skill = %s, location = %s WHERE emp_id = %s"
+    cursor = db_conn.cursor()
+    
+    try:
+        cursor.execute(update_sql, (first_name, last_name, pri_skill, location, emp_id))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+        # Uplaod image file in S3 #
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data updated in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("All modification done...")
+    return render_template('UpdateEmp.html', name=emp_name)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
